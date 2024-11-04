c7d5904a256f16b8d7e1243b5074cfcc44b511da
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-11-04 14:19:30 +0000

    Header updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import RapidsLogo from './assets/RapidsLogo.jpg'
 import Header from './components/Header'
 import HomeContainer from './containers/HomeContainer'
 
@@ -9,7 +8,6 @@ function App() {
 
   return (
     <>
-    <h1>Kelvin Rapids</h1>
     <BrowserRouter>
       <Header/>
       <Routes>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,24 @@
+import RapidsLogo from '../assets/RapidsLogo.jpg'
+import { Link } from 'react-router-dom'
+
+
+
+
+
 export default function Header(){
     return(
-        <h2>Header</h2>
+        <div className="header">
+            <div className='title'>
+            <img className="logo" src = {RapidsLogo}/>
+            <h1>Kelvin Rapids Dodgeball Club</h1>
+            </div>
+            <ul className="links">
+                <Link to="/about">About</Link>
+                <Link to="/team">Meet the Team</Link>
+                <Link to="/News">Club News</Link>
+                <Link to="/find">Find Us</Link>
+                <Link to="/contact">Contact</Link>
+            </ul>
+    </div>
     )
 }
