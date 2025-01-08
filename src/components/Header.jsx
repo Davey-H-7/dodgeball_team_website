949c4b3c6f5dcb19cb4468efe3a6c3795204cfe9
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2025-01-08 16:41:16 +0000

    updated styling and added social links to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import RapidsLogo from '../assets/RapidsLogo.jpg'
+import InstagramLogo from '../assets/instagram.png'
+import FacebookLogo from '../assets/facebook.png'
 import { Link } from 'react-router-dom'
 import '../styling/Header.css'
 
@@ -10,14 +12,17 @@ export default function Header(){
         <div className="header">
             <div className='title'>
             <a href ="/"><img className="logo" src = {RapidsLogo}/></a>
+                <div className="socialLinks">
+                    <a href = "https://www.instagram.com/kelvinrapids/" ><img className='socialLogo' src = {InstagramLogo}/></a>
+                    <a href = "https://www.facebook.com/search/top?q=kelvin%20rapids%20dodgeball%20club" ><img className='socialLogo' src = {FacebookLogo}/></a>
+                </div>
             <h1>Kelvin Rapids Dodgeball Club</h1>
             </div>
             <ul className="links">
                 <Link to="/about">About</Link>
                 <Link to="/team">Meet the Team</Link>
-                <Link to="/News">Club News</Link>
+                <Link to="/Results">Past Results</Link>
                 <Link to="/find">Find Us</Link>
-                <Link to="/contact">Contact</Link>
             </ul>
     </div>
     )
