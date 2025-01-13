8931a59ef08ee6ab40ee53ef30256a26082febab
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2025-01-13 15:13:01 +0000

    updating header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,13 +10,15 @@ import '../styling/Header.css'
 export default function Header(){
     return(
         <div className="header">
-            <div className='title'>
-            <a href ="/"><img className="logo" src = {RapidsLogo}/></a>
+            <div className='logoTitleSocials'>
+                <div className='logoTitle'>
+                    <a href ="/"><img className="logo" src = {RapidsLogo}/></a>
+                    <h1>Kelvin Rapids Dodgeball Club</h1>
+                </div>
                 <div className="socialLinks">
                     <a href = "https://www.instagram.com/kelvinrapids/" ><img className='socialLogo' src = {InstagramLogo}/></a>
                     <a href = "https://www.facebook.com/search/top?q=kelvin%20rapids%20dodgeball%20club" ><img className='socialLogo' src = {FacebookLogo}/></a>
                 </div>
-            <h1>Kelvin Rapids Dodgeball Club</h1>
             </div>
             <ul className="links">
                 <Link to="/about">About</Link>
